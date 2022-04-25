dc9d20c6a63aa41df41e000b19effa9e440a9735
Radharani Valongo <radpursuitmedia@gmail.com>
2022-04-25 16:13:08 -0400

    added query filtering

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express(); 
 const bookController = require('./controllers/bookController');
 
-app.use('/', bookController);
+
+app.use('/books', bookController);
 
 
 module.exports = app;
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,14 +4,34 @@ const router = express.Router();
 const bookData = require("../bookData.json");
 
 router.get("/", (request,response) => {
-    response.json({bookData})
+    let books = bookData.books
+    let {start, end, title} = request.query;
+    start = Number(start), end = Number(end);
+    console.log(start,end)
+    if(start && end && title){
+        books = books.filter(book => book.id >= start && book.id <= end && book.title.toLowerCase().includes(title))
+    } else if(start && end){
+        books = books.filter(book => book.id >= start && book.id <= end)
+    }  else if(title){
+        books = books.filter(book => book.title.toLowerCase().includes(title))
+    }
+    response.json(books)
 })
 
 router.get("/:id", (request,response) => {
-    const bookId = Number(request.params.id)
-    const books = bookData
-    const singleBook = books.find(book => book.id === bookId  )
-    response.json(singleBook)
+    const bookId = request.params.id
+    if(!/[0-9]/.test(bookId)){
+        throw "Book ID must be an integer";
+    };
+
+    const books = bookData.books;
+    const singleBook = books.find(book => book.id === bookId);
+
+    if(singleBook){
+        response.json(singleBook)
+    } else {
+        throw "No book with this ID found"
+    }
 })
 
 
