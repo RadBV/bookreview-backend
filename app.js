e8b2233e5eac398ef8eaf1caf31d80f3f29203bb
Radharani Valongo <radpursuitmedia@gmail.com>
2022-05-13 13:41:25 -0400

    switched cors back to server.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express(); 
 const bodyParser = require('body-parser')
 const bookController = require('./controllers/bookController');
-const cors = require("cors");
 
-app.use(cors())
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/books', bookController);
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const app = require("./app");
-
+const cors = require("cors");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 
 
-
+app.use(cors())
 app.listen(PORT, () => {
     console.log(`Your app is running at ${PORT}`);
 });
